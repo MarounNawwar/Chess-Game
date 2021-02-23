972c8a557ec71d2ef722585fe5c7f5d23e8b46b8
MarounNawwar <68354149+MarounNawwar@users.noreply.github.com>
2021-02-23 22:43:59 +0200

    Major Updates
    Edited the Movement Interface and Added the Checkmate feature.

---
+++ ChessProject/src/Pieces/Piece.java
@@ -0,0 +1,73 @@
+package Pieces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Board.Board;
+import Position.Location;
+import javafx.scene.image.*;
+import squares.Square;
+
+public abstract class Piece implements Movable{
+	protected String name;
+	protected PieceColor pieceColor;
+	protected Square currentSquare;
+	protected Image image;
+	
+	//Constructor 
+	public Piece(PieceColor pieceColor) {
+		this.pieceColor = pieceColor;
+	}
+	
+	
+	@Override
+	public void makeMove(Square destinationSquare) {
+		if(this.getValidMoves(Board.getInstance()).contains(destinationSquare.getLocation())) {
+			this.getCurrSquare().releasePiece();
+			this.setCurrSquare(destinationSquare);
+			destinationSquare.SetCurrPiece(this);
+		}
+	}
+	
+	//Return the moves that are available in the targetSquares list and that are valid 
+	@Override
+	public List<Location> getValidMoves(Board board, List<Location> targetSquares) {
+		List<Location> validMoves = new ArrayList<>();
+		List<Location> possibleMoves = this.getValidMoves(board);
+			
+		for(Location loc : possibleMoves)
+			if(targetSquares.contains(loc))
+				validMoves.add(loc);
+
+		return validMoves;
+	}
+	
+	public int getRow() {
+		return this.getCurrSquare().getRow();
+	}
+	
+	public int getColumn() {
+		return this.getCurrSquare().getColumn();
+	}
+	
+	//Getters and Setters
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public PieceColor getPieceColor() {
+		return this.pieceColor;
+	}
+	
+	public Square getCurrSquare() {
+		return this.currentSquare;
+	}
+	
+	public void setCurrSquare(Square currSquare) {
+		this.currentSquare = currSquare;
+	}
+
+	public abstract Image getImage();
+	
+}
