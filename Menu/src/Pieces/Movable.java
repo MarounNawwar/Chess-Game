8c755f396bbf8ad853983ff3fdf40b4f54c61dfe
nicole1290 <79148566+nicole1290@users.noreply.github.com>
2021-02-24 02:29:21 +0200

    Add files via upload

---
+++ Menu/src/Pieces/Movable.java
@@ -0,0 +1,15 @@
+package Pieces;
+
+import java.util.List;
+
+import Position.Location;
+import squares.Square;
+import Board.Board;
+
+public interface Movable {
+	
+	List<Location> getValidMoves(Board board);
+	List<Location> getValidMoves(Board board, List<Location> targetSquares);
+	void makeMove(Square square);
+	
+}
---
+++ Menu/src/application/SampleController.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.awt.Event;
+import java.io.IOException;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+public class SampleController {
+	
+
+	
+}
